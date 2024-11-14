7569a29797b4e8b6e70db2ddc187027156e11d0f
itz-koi <longnguyen0397042@gmail.com>
2024-11-14 13:34:27 +0700

    get JWT

--- src/main/java/org/example/miniforum/repository/UserRepository.java
+++ src/main/java/org/example/miniforum/repository/UserRepository.java
@@ -4,8 +4,10 @@ import org.example.miniforum.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     public boolean existsByUsername(String username);
-    public User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
--- src/main/java/org/example/miniforum/service/UserService.java
+++ src/main/java/org/example/miniforum/service/UserService.java
@@ -11,10 +11,13 @@ import org.example.miniforum.mapper.UserMapper;
 import org.example.miniforum.model.User;
 import org.example.miniforum.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -31,7 +34,8 @@ public class UserService {
 
     public UserResponse createUser(UserRequest userRequest) {
         User user = userMapper.toUser(userRequest);
-
+        PasswordEncoder passwordEncoder= new BCryptPasswordEncoder(10);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userMapper.toUserResponse(userRepository.save(user));
     }
 
@@ -39,7 +43,8 @@ public class UserService {
 
         if(userRepository.existsByUsername(userLogin.getUsername())){
 
-            User user = userRepository.findByUsername(userLogin.getUsername());
+            User user = userRepository.findByUsername(userLogin.getUsername())
+                    .orElseThrow(()-> new AppException(ErrorCode.USER_NOT_EXIST));
 
             if(userLogin.getPassword().equals(user.getPassword())){
                 LoginResponse loginResponse = new LoginResponse();
@@ -66,4 +71,7 @@ public class UserService {
         log.info("user email: {}", user.getEmail());
         return userMapper.toUserResponse(userRepository.save(user));
     }
+
+
+
 }
